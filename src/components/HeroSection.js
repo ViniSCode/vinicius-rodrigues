244dd6b4995cbe8b6ddf682457d4215560327f40
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-01-23 23:50:24 -0300

    testing animatino

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 
-export function HeroSection ({section1Ref, inViewFadeOut, opacitySection1}) {
+export function HeroSection ({section1Ref, inViewFadeOut, opacitySection1, heroHiFade, heroTitleFade, heroSubFade, heroPFade, heroButtonFade, heroImageFade,}) {
   return (
-    <motion.section className="text-white lg:flex items-center justify-between" ref={section1Ref} initial={inViewFadeOut} animate={opacitySection1}>
+    <motion.section className="text-white lg:flex items-center justify-between" ref={section1Ref}>
       <div className='lg:max-w-[600px] xl:max-w-[782px]'>
-        <p className="text-blue-gradient font-montserrat font-bold text-xs md:text-base">Hi, my name is</p>
+        <motion.p className="text-blue-gradient font-montserrat font-bold text-xs md:text-base" initial={inViewFadeOut} animate={heroHiFade}>Hi, my name is</motion.p>        
         <h1 className="mt-4 font-sans font-extrabold text-[32px] md:text-[58px] lg:text-[52px] xl:text-[62px]">Vinicius Rodrigues</h1>
         <h2 className="mt-4 font-sans font-bold text-[20px] text-blue-gradient md:text-[38px] lg:text-[34px] xl:text-[50px]">Creating solutions with code</h2>
         <p className="lg:max-w-[510px] xl:max-w-[700px] xl:text-base mt-6 font-montserrat font-semibold text-xs text-gray-250 md:text-sm lg:text-xs">As a passionate technology enthusiast and <span className="text-blue-500">front-end</span> developer 
--- src/pages/home.js
+++ src/pages/home.js
@@ -14,20 +14,50 @@ export default function Home () {
   const {ref: section5Ref, inView: section5View } = useInView({ triggerOnce: true });
 
   const opacitySection1 = useAnimation();
+  const headerFade = useAnimation();
   const opacitySection2 = useAnimation();
   const opacitySection3 = useAnimation();
   const opacitySection4 = useAnimation();
   const opacitySection5 = useAnimation();
+
+  const heroHiFade = useAnimation();
+  const heroTitleFade = useAnimation();
+  const heroSubFade = useAnimation();
+  const heroPFade = useAnimation();
+  const heroButtonFade = useAnimation();
+  const heroImageFade = useAnimation();
   
-  const inViewFadeIn = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.2, delayChildren: 0.5}, opacity: 1}
+  const inViewFadeIn = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.2}, opacity: 1}
   const inViewFadeOut = { transition: { duration: 0.5, ease: 'easeInOut'}, opacity: 0 }
+ 
+  const inViewXFadeIn = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.3}, opacity: 1, x: '0'}
+  const inViewXFadeIn04 = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.4}, opacity: 1, x: '0'}
+  const inViewXFadeIn05 = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.5}, opacity: 1, x: '0'}
+  const inViewXFadeIn06 = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.6}, opacity: 1, x: '0'}
+  const inViewXFadeIn07 = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.7}, opacity: 1, x: '0'}
+  const inViewXFadeIn08 = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.8}, opacity: 1, x: '0'}
+  const inViewXFadeIn09 = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.9}, opacity: 1, x: '0'}
+  const inViewXFadeOut = { transition: { duration: 0.5, ease: 'easeInOut'}, opacity: 0, x: '-200px' }
+ 
+  const inViewYFadeIn = { transition: { duration: 0.5, ease: 'easeInOut', delay: 0.3}, opacity: 1, y: '0'}
+  const inViewYFadeOut = { transition: { duration: 0.5, ease: 'easeInOut'}, opacity: 0, y: '200px' }
+
+
 
   useEffect(() => {
     if (section1View) {
       opacitySection1.start(inViewFadeIn);
+      headerFade.start(inViewFadeIn);
+      heroHiFade.start(inViewXFadeIn04)
+      heroTitleFade.start(inViewXFadeIn05)
+      heroSubFade.start(inViewXFadeIn06)
+      heroPFade.start(inViewXFadeIn07)
+      heroButtonFade.start(inViewXFadeIn08)
+      heroImageFade.start(inViewXFadeIn09)
     } 
     if (!section1View) {
       opacitySection1.start(inViewFadeOut);
+      headerFade.start(inViewFadeOut);
     }
     if (section2View) {
       opacitySection2.start(inViewFadeIn);
@@ -58,9 +88,9 @@ export default function Home () {
   }, [section1View, section2View, section3View, section4View, section5View]);
   
   return (
-    <div className="container snap-start snap-mandatory scroll-smooth mx-auto">
+    <motion.div className="container snap-start snap-mandatory scroll-smooth mx-auto"  animate={{opacity: 1}} initial={{opacity: 0}} transition={{delay: 0.1}}>
       <div className="lg:hidden menu-bg-blue-gradient fixed top-0 left-0 right-0 shadow-sm z-[20]">
-        <header className="pt-[22px] pb-[22px] w-full max-w-[358px] md:max-w-[628px] px-4 mx-auto">
+        <header  className="pt-[22px] pb-[22px] w-full max-w-[358px] md:max-w-[628px] px-4 mx-auto">
           <nav className="flex items-center justify-between">
             <motion.span  className="font-montserrat font-bold text-sm leading-[17px] tracking-[-0.02em] text-blue-gradient">VINÍCIUS RODRIGUES</motion.span>
             <img src="/assets/menu.svg" alt="" />
@@ -85,6 +115,12 @@ export default function Home () {
           inViewFadeOut={inViewFadeOut}
           section1Ref={section1Ref}
           opacitySection1={opacitySection1} 
+          heroHiFade={heroHiFade}
+          heroTitleFade={heroTitleFade}
+          heroSubFade={heroSubFade}
+          heroPFade={heroPFade}
+          heroButtonFade={heroButtonFade}
+          heroImageFade={heroImageFade}
         />
 
         <AboutSection 
@@ -123,6 +159,6 @@ export default function Home () {
           </div>
         </motion.section>  
       </main>
-    </div>
+    </motion.div>
   )
 }
